ed703af88a722ff68ffe302517c7c72b03d2f289
jardan72 <jarledanielsen@hotmail.com>
2024-04-30 20:39:33 +0200

    tiles added

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Line, Rectangle
+from kivy.graphics import Color, Line, Quad
 from kivy.properties import NumericProperty, Clock
 from kivy.config import Config
 from kivy.utils import platform
-
 from kivy.config import Config
 Config.set('graphics', 'resizable', 0)
 
@@ -13,29 +12,39 @@ Window.size = (900, 400)
 
 
 class MainWidget(Widget):
+    from transforms import transform, transform_2D, transform_perspective
+    from user_actions import keyboard_closed, on_keyboard_down, on_keyboard_up, on_touch_down, on_touch_up
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-    V_NB_LINES = 10
-    V_LINES_SPACING = 0.25   # percentage in screen width
+    V_NB_LINES = 4
+    V_LINES_SPACING = 0.1   # percentage in screen width
     H_NB_LINES = 15
     H_LINES_SPACING = 0.1   # percentage in screen height
 
     vertical_lines = []
     horizontal_lines = []
 
-    SPEED_Y = 4
+    SPEED_Y = 1
     current_offset_y = 0
 
     SPEED_X = 12
     current_speed_x = 0
     current_offset_x = 0
 
+    NB_TILES = 8
+    tiles = []
+    tiles_coordinates = []
+
+    current_y_loop = 0
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # print("Init W: " + str(self.width) + " Init H: " + str(self.height))
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        self.init_tiles()
+        self.generate_tiles_coordinates()
         if self.is_desktop():
             self._keyboard = Window.request_keyboard(
                 self.keyboard_closed, self)
@@ -44,35 +53,24 @@ class MainWidget(Widget):
 
         Clock.schedule_interval(self.update, 1/60)
 
-    def keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        self._keyboard.unbind(on_key_up=self.on_keyboard_up)
-        self._keyboard = None
-
     def is_desktop(self):
         if platform in ('linux', 'win', 'macosx'):
             return True
         return False
 
-    def on_parent(self, widget, parent):
-        # print("Parent W: " + str(self.width) + " Parent H: " + str(self.height))
-        pass
+    # def on_parent(self, widget, parent):
+    #     # print("Parent W: " + str(self.width) + " Parent H: " + str(self.height))
+    #     pass
 
-    def on_size(self, *args):
-        # print("ON_SIZE W: " + str(self.width) + " H: " + str(self.height))
-        # self.perspective_point_x = self.width / 2
-        # self.perspective_point_y = self.height * 0.75
-        # self.update_vertical_lines()
-        # self.update_horizontal_lines()
-        pass
+    def init_tiles(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.NB_TILES):
+                self.tiles.append(Quad())
 
-    def on_perspective_point_x(self, widget, value):
-        # print("PX: " + str(value))
-        pass
-
-    def on_perspective_point_y(self, widget, value):
-        # print("PY: " + str(value))
-        pass
+    def generate_tiles_coordinates(self):
+        for i in range(0, self.NB_TILES):
+            self.tiles_coordinates.append((0, i))
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -80,17 +78,52 @@ class MainWidget(Widget):
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
-    def update_vertical_lines(self):
-        center_line_x = int(self.width / 2)
+    def get_line_x_from_index(self, index):
+        center_line_x = self.perspective_point_x
         spacing = self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES / 2) + 0.5
-        for i in range(0, self.V_NB_LINES):
-            line_x = center_line_x + offset*spacing + self.current_offset_x
+        offset = index - 0.5
+        line_x = center_line_x + offset*spacing + self.current_offset_x
+        return line_x
+
+    def get_line_y_from_index(self, index):
+        spacing_y = self.H_LINES_SPACING * self.height
+        line_y = index * spacing_y - self.current_offset_y
+        return line_y
+
+    def get_tile_coordinates(self, ti_x, ti_y):
+        ti_y = ti_y - self.current_y_loop
+        x = self.get_line_x_from_index(ti_x)
+        y = self.get_line_y_from_index(ti_y)
+        return x, y
+
+    def update_tiles(self):
+        for i in range(0, self.NB_TILES):
+            tile = self.tiles[i]
+            tile_coordinates = self.tiles_coordinates[i]
+            xmin, ymin = self.get_tile_coordinates(
+                tile_coordinates[0], tile_coordinates[1])
+            xmax, ymax = self.get_tile_coordinates(
+                tile_coordinates[0]+1, tile_coordinates[1]+1)
+
+            # 2     3
+            #
+            # 1     4
+
+            x1, y1 = self.transform(xmin, ymin)
+            x2, y2 = self.transform(xmin, ymax)
+            x3, y3 = self.transform(xmax, ymax)
+            x4, y4 = self.transform(xmax, ymin)
+
+            tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
+
+    def update_vertical_lines(self):
+        start_index = -int(self.V_NB_LINES/2)+1
+        for i in range(start_index, start_index+self.V_NB_LINES):
+            line_x = self.get_line_x_from_index(i)
 
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
-            offset += 1
 
     def init_horizontal_lines(self):
         with self.canvas:
@@ -99,62 +132,16 @@ class MainWidget(Widget):
                 self.horizontal_lines.append(Line())
 
     def update_horizontal_lines(self):
-        center_line_x = int(self.width / 2)
-        spacing = self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES / 2) + 0.5
-
-        xmin = center_line_x + offset*spacing + self.current_offset_x
-        xmax = center_line_x - offset*spacing + self.current_offset_x
-        spacing_y = self.H_LINES_SPACING * self.height
+        start_index = -int(self.V_NB_LINES/2)+1
+        end_index = start_index+self.V_NB_LINES - 1
+        xmin = self.get_line_x_from_index(start_index)
+        xmax = self.get_line_x_from_index(end_index)
 
         for i in range(0, self.H_NB_LINES):
-            line_y = i * spacing_y - self.current_offset_y
+            line_y = self.get_line_y_from_index(i)
             x1, y1 = self.transform(xmin, line_y)
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
-
-    def transform(self, x, y):
-        # return self.transform_2D(x, y)
-        return self.transform_perspective(x, y)
-
-    def transform_2D(self, x, y):
-        return int(x), int(y)
-
-    def transform_perspective(self, x, y):
-        lin_y = y*self.perspective_point_y / self.height
-        if lin_y > self.perspective_point_y:
-            lin_y = self.perspective_point_y
-        diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - lin_y
-        factor_y = diff_y / self.perspective_point_y
-        factor_y = pow(factor_y, 4)
-        tr_x = self.perspective_point_x + diff_x*factor_y
-        tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
-        return int(tr_x), int(tr_y)
-
-    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        keynr, key = keycode
-        # print("keycode: " + str(key))
-        if keycode[1] == 'left':
-            self.current_speed_x = self.SPEED_X
-        elif keycode[1] == 'right':
-            self.current_speed_x = -self.SPEED_X
-        return True
-
-    def on_keyboard_up(self, keyboard, keycode):
-        self.current_speed_x = 0
-        return True
-
-    def on_touch_down(self, touch):
-        if touch.x < self.width/2:
-            self.current_speed_x = self.SPEED_X
-            # print("<-")
-        else:
-            self.current_speed_x = - self.SPEED_X
-
-    def on_touch_up(self, touch):
-        self.current_speed_x = 0
-        # print("UP")
 
     def update(self, dt):
         # print("dt: "+str(dt)+" 1/60:"+str(1/60))
@@ -162,12 +149,16 @@ class MainWidget(Widget):
         time_factor = dt*60
         self.update_horizontal_lines()
         self.update_vertical_lines()
+        self.update_tiles()
         self.current_offset_y += self.SPEED_Y*time_factor
+
         spacing_y = self.H_LINES_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
+            self.current_y_loop += 1
+            # print("loop: "+ str(self.current_y_loop))
 
-        self.current_offset_x += self.current_speed_x*time_factor
+        # self.current_offset_x += self.current_speed_x*time_factor
 
 
 class GalaxyApp(App):
